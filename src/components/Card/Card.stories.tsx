6c6a2d27a00c0096a8ce77faa0c3dbfeb3d2ba94
Nick Galante <nicholasgalante1997@gmail.com>
2023-08-11 19:00:52 -0600

    coming up next, futurama

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -48,3 +48,33 @@ export const FullSmall: CardStory = {
   },
   render: (args: CardProps) => <CardComponent {...args} />
 };
+
+export const MiniLarge: CardStory = {
+  args: {
+    type: 'mini',
+    size: 'lg',
+    alt: 'Doodles 2',
+    title: 'Doodles: A Case Study In NFTs',
+    description: 'What is Doodles? Doodles is a collection of NFT\'s (Non-Fungible Tokens) that has emerged as a result of the past wave of interest in NFT baedased trading, as an alternative to traditional means of trading.',
+    cta: {
+      href: '#',
+      text: 'Read More'
+    }
+  },
+  render: (args: CardProps) => <CardComponent {...args} />
+};
+
+export const MiniSmall: CardStory = {
+  args: {
+    type: 'mini',
+    size: 'sm',
+    alt: 'Doodles 2',
+    title: 'Doodles: A Case Study In NFTs',
+    description: 'What is Doodles? Doodles is a collection of NFT\'s (Non-Fungible Tokens) that has emerged as a result of the past wave of interest in NFT baedased trading, as an alternative to traditional means of trading.',
+    cta: {
+      href: '#',
+      text: 'Read More'
+    }
+  },
+  render: (args: CardProps) => <CardComponent {...args} />
+};
